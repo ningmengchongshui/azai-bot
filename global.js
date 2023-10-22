1aeed4b312709f9cd74e57ade2422d869976cb30
柠檬冲水 <916415899@qq.com>
2023-10-22 20:59:09 +0800

    更新

--- global.js
+++ global.js
@@ -1,4 +1,8 @@
-// 设置一些yunzai的全局变量
+/**
+ * ************************
+ * 设置一些yunzai的全局变量
+ * ************************
+ */
 const logger = console
 logger.red = val => val
 logger.debug = val => val
@@ -6,42 +10,95 @@ logger.yellow = val => val
 logger.mark = val => val
 logger.green = val => val
 global.logger = logger
-
 import { Redis as redis } from './db/redis/index.js'
 import { getPathBuffer } from 'alemonjs'
 import plugin from './lib/plugins/plugin.js'
-
+import { existsSync } from 'fs'
+import { join } from 'path'
+/**
+ * yunzai
+ */
 global.plugin = plugin
-
 global.redis = redis
-
+/**
+ * ars
+ */
+const ars = process.argv.slice(2)
+/**
+ * icqq
+ */
 const segment = {
-  // 如果是 bufeer
+  /**
+   * 图片
+   * @param {*} val
+   * @returns
+   */
   image: val => {
+    // 直接buffer
     if (Buffer.isBuffer(val)) return val
-    // 如果地址
-    if (typeof val == 'string') return getPathBuffer(val)
+    // 相对于机器人地址的
+    const add = join(process.cwd(), val)
+    if (existsSync(add)) getPathBuffer(val)
+    // 直接全地址的
+    if (existsSync(val)) Buffer.from(val)
+    // 未知的
     return val
   },
-  //
+  /**
+   * 视频
+   * @param {*} val
+   * @returns
+   */
   video: val => `[video](${val})`,
+  /**
+   * at用户
+   * @param {*} uid
+   * @returns
+   */
   at: uid => {
-    // villa
-    if (uid == 'all') return `<@!everyone>`
-    // qq
-    //  <@${uid}>  || `<@everyone>`
-    return `<@!${uid}>`
+    if (ars.indexOf('villa')) {
+      if (uid == 'all') return `<@!everyone>`
+      return `<@!${uid}>`
+    }
+    if (ars.indexOf('qq')) {
+      if (uid == 'all') return `<@everyone>`
+      return `<@${uid}>`
+    }
+    if (ars.indexOf('ntqq')) {
+      if (uid == 'all') return `@everyone`
+      return `<@${uid}>`
+    }
+    if (ars.indexOf('kook')) {
+      if (uid == 'all') return `(met)all(met)`
+      return `(met)${uid}(met)`
+    }
+    if (ars.indexOf('discord')) {
+      if (uid == 'all') return `@everyone`
+      return `<@${uid}>`
+    }
+    return ''
   },
+  /**
+   * 语音
+   * @param {*} val
+   * @returns
+   */
   record: val => {
+    // 直接buffer
     if (Buffer.isBuffer(val)) return val
-    // 如果地址
-    if (typeof val == 'string') return getPathBuffer(val)
+    // 相对于机器人地址的
+    const add = join(process.cwd(), val)
+    if (existsSync(add)) getPathBuffer(val)
+    // 直接全地址的
+    if (existsSync(val)) Buffer.from(val)
+    // 未知的
     return val
   }
 }
-
 global.segment = segment
-
+/**
+ * icqq
+ */
 const Bot = {
   uin: '',
   logger,
@@ -57,5 +114,4 @@ const Bot = {
     get: val => val
   }
 }
-
 global.Bot = Bot
