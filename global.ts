bd8318c5b15f94cf4282790a7472b4382f621d19
柠檬冲水 <916415899@qq.com>
2023-10-24 01:18:59 +0800

    更新

--- global.ts
+++ global.ts
@@ -28,8 +28,8 @@ const ars = process.argv.slice(2)
 /**
  * 必要目录
  */
-mkdirSync('./temp/html')
-mkdirSync('./resources')
+mkdirSync('./temp/html/', { recursive: true })
+mkdirSync('./resources', { recursive: true })
 /**
  * icqq
  */
