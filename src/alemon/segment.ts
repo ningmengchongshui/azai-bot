532ff4824aaaf0f91dc1aae1605b06df0ba2e70e
柠檬冲水 <916415899@qq.com>
2023-11-03 22:15:15 +0800

    更新

--- src/alemon/segment.ts
+++ src/alemon/segment.ts
@@ -34,25 +34,23 @@ const segment = {
    * @returns
    */
   at: (uid: string) => {
-    if (ars.includes('villa')) {
-      if (uid == 'all') return `<@!everyone>`
-      return `<@!${uid}>`
-    }
-    if (ars.includes('qq')) {
+    if (
+      ars.includes('villa') ||
+      ars.includes('qq') ||
+      ars.includes('one') ||
+      ars.includes('discord')
+    ) {
       if (uid == 'all') return `<@everyone>`
       return `<@${uid}>`
     }
     if (ars.includes('ntqq')) {
       if (uid == 'all') return `@everyone`
-      return `<@${uid}>`
+      // ntqq 没有at
+      return ``
     }
     if (ars.includes('kook')) {
       if (uid == 'all') return `(met)all(met)`
       return `(met)${uid}(met)`
-    }
-    if (ars.includes('discord')) {
-      if (uid == 'all') return `@everyone`
-      return `<@${uid}>`
     }
     return ''
   },
