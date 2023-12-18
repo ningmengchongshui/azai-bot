045b492e906170b3076fc0d8d2135ffdb90ae93d
柠檬冲水 <916415899@qq.com>
2023-12-18 13:55:00 +0800

    简化

--- plugins/other/main.js
+++ plugins/other/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'alemonjs'
 import * as apps from './restart.js'
 createApp(import.meta.url).reSetEvent(async e => {
-  if (e.attribute == 'group') {
-    e.isGroup = true
- }
+  if (e.attribute == 'group') e.isGroup = true
   await runtime.init(e)
   e.sender = {}
   e.sender.card = e.user_name
