a78d92ee57169fcf618e6ebc946a31c1ecd4487e
柠檬冲水 <916415899@qq.com>
2023-12-18 16:48:55 +0800

    隔离原神功能

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { existsSync } from 'fs'
+import { join } from 'path'
+import { importPath } from 'alemonjs'
 /**
  * *************
  * 自定义时间提示
@@ -6,10 +9,25 @@
 import './alemon/logs.js'
 /**
  * *************
- * Yunzai global
+ * A-Yunzai global
  * *************
  */
 import './global.js'
+/**
+ * *********
+ * yunzai.global
+ * *********
+ */
+import './yunzai/ayunzai.js'
+/**
+ * *********
+ * yunzai.runtime
+ * *********
+ */
+const pathToFile = './yunzai/runtime.ts'
+if (existsSync(join(importPath(import.meta.url).cwd(), pathToFile))) {
+  await import(pathToFile)
+}
 /**
  * ***********
  * segment
---
+++ src/yunzai/ayunzai.ts
@@ -0,0 +1,69 @@
+/**
+ * *********
+ * yunzai
+ * *********
+ */
+import Plugins from '../../lib/plugins/plugin.js'
+import renderer from '../../lib/renderer/Renderer.js'
+declare global {
+  var plugin: typeof Plugins
+}
+global.plugin = Plugins
+declare global {
+  var Renderer: typeof renderer
+}
+global.Renderer = renderer
+/**
+ * V2转义成V3
+ * @param rule V2指令对象
+ * @param sourceObject v2插件文件对象
+ * @returns calss
+ */
+const assignPropertiesAndMethods = (rules: any, sourceObject: any) => {
+  const rule = Object.keys(rules).map(item => ({
+    ...rules[item],
+    fnc: item
+  }))
+  class APP extends plugin {
+    constructor() {
+      super({
+        rule
+      })
+    }
+  }
+  const propertyNames = Object.getOwnPropertyNames(sourceObject)
+  propertyNames.forEach(propertyName => {
+    const descriptor = Object.getOwnPropertyDescriptor(
+      sourceObject,
+      propertyName
+    )
+    if (descriptor) {
+      Object.defineProperty(APP.prototype, propertyName, descriptor)
+    }
+  })
+  return APP
+}
+declare global {
+  var YUNZAIV2: typeof assignPropertiesAndMethods
+}
+global.YUNZAIV2 = assignPropertiesAndMethods
+
+/**
+ *
+ * @param e
+ * @returns
+ */
+async function reSetEvent(e: any) {
+  if (e.attribute == 'group') e.isGroup = true
+  e.isSr = true
+  e.isGs = true
+  e.sender = {}
+  e.game === 'sr'
+  e.sender.card = e.user_name
+  e.checkAuth = (val: any) => val
+  return e
+}
+declare global {
+  var YUNZAI_EVENT: typeof reSetEvent
+}
+global.YUNZAI_EVENT = reSetEvent
