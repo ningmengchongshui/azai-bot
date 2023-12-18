5e45a4a2b05b2593256f080e8aac504a936baf02
柠檬冲水 <916415899@qq.com>
2023-12-18 11:09:49 +0800

    更改版本

--- afloat.config.ts
+++ afloat.config.ts
@@ -3,20 +3,7 @@ export default defineAfloat({
   nodemon: {
     watch: ['a.*.{ts,js}', 'src', 'db'],
     env: {
-      // discord
-      DISCORD_API_DATA: 'dev',
-      // kook
-      KOOK_API_DATA: 'dev',
-      // villa
-      VILLA_API_DATA: 'dev',
-      // qq
-      // ntqq
-      NTQQ_API_DATA: 'dev',
-      // event
-      ALEMONJS_EVENT: 'dev',
-      // e
-      ALEMONJS_MESSAGE: 'dev',
-      NODE_ENV: 'development'
+      // 自动读取.env文件
     }
   }
 })
