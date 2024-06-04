49e9366c7ac26976935b06896a41d86f583c8735
ningmengchongshui <916415899@qq.com>
2024-06-04 20:36:40 +0800

    简化

--- afloat.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineAfloat } from 'afloat'
-export default defineAfloat({
-  nodemon: {
-    watch: ['alemon.*.{ts,js}', 'lib'],
-    env: {
-      // 自动读取.env文件
-    }
-  }
-})
--- alemon.config.ts
+++ alemon.config.ts
@@ -1,10 +1,19 @@
 import './lib/src/index.js'
+import { join } from 'path'
+import { existsSync } from 'fs'
 import { defineConfig } from 'alemonjs'
+const dir = join(process.cwd(), 'dist/main.js')
+const scripts =
+  process.env.NODE_ENV != 'production' && !existsSync(dir)
+    ? 'src/main.ts'
+    : 'dist/main.js'
+console.info('[APP] 测试 启动', scripts)
 export default defineConfig({
   // 个人应用
   app: {
     init: false
   },
+  scripts,
   // 事件过滤
   shieldEvent: ['poke', 'notice.*.poke']
 })
